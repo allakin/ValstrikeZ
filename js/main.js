8d02ed820898c4168bc973fc543e65a22ea044e4
Pavel <19paul91@mail.ru>
2014-10-04 14:50:38 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        nextSelector: '#slider-next',
+        prevSelector: '#slider-prev',
+        nextText: '',
+        pager: false,
+        auto: true,
+        prevText: ''
+    });
+});
