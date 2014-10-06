5e12715cca8f48e7677a28b88092e466652ce4bc
Pavel <19paul91@mail.ru>
2014-10-06 17:13:32 +0400

    Почти готов слайдер типа карусель

--- js/main.js
+++ js/main.js
@@ -19,3 +19,13 @@ $(document).ready(function(){
         prevText: ''
     });
 });
+
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        slideWidth: 940,
+        minSlides: 1,
+        maxSlides: 2,
+        slideMargin: 10,
+        mode: 'fade'
+    });
+});
